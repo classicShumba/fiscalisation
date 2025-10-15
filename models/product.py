5b2bed33ae27d0a99891247b101d34342b5d3924
classicShumba <allan@classicshumba.co.zw>
2025-10-15 09:30:54 +0200

    Refactor HS Code field definition and update product view XML for consistency

--- models/product.py
+++ models/product.py
@@ -3,9 +3,18 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    hs_code = fields.Char(string='HS Code', help='Harmonized System Code for the product')
+    hs_code = fields.Char(
+        string='HS Code', 
+        help='Harmonized System Code for the product'
+    )
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    hs_code = fields.Char(string='HS Code', help='Harmonized System Code for the product')
+    hs_code = fields.Char(
+        string='HS Code',
+        related='product_tmpl_id.hs_code',
+        store=True,  # Optional: stores the value in database for faster searches
+        readonly=False,  # Allows editing from product variant
+        help='Harmonized System Code for the product'
+    )
